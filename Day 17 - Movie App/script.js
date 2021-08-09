4d41ff14dafa81e2861e03cc65accf02a3c1c001
Suprememajor <ndipnobertetta@gmail.com>
2021-08-09 10:11:21 +0100

    Day 17 - Movie App

---
+++ Day 17 - Movie App/script.js
@@ -0,0 +1,61 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=080364b142456f08ae24ad62c7383fb8&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=080364b142456f08ae24ad62c7383fb8&query="';
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { overview, poster_path, title, vote_average} = movie;
+
+        const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}            
+        </div>
+    `;
+    main.append(movieEl);
+    });    
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm);
+
+        search.value = '';
+    }else {
+        window.location.reload();
+    }
+});
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if(vote >= 5) {
+        return 'orange';
+    }else {
+        return 'red';
+    }
+}
